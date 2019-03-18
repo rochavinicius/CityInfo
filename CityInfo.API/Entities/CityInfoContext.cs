0db83c6b260b1df77c69dd020a3eec2a5482ff74
VINÍCIUS BORBA DA ROCHA <16280281@pucrs.br>
2019-03-18 18:33:06 -0300

    continuing

---
+++ CityInfo.API/Entities/CityInfoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class CityInfoContext : DbContext
+    {
+        public CityInfoContext(DbContextOptions<CityInfoContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        DbSet<City> Cities { get; set; }
+
+        DbSet<PointOfInterest> PointsOfInterest { get; set; }
+    }
+}
